c5f6c6916d7a41d85a3c441ba6acf2337c0f137a
Sap ★ <samuel.newmark10@gmail.com>
2021-02-12 14:08:21 -0600

    Simplify tree build logic

--- src/logic/tree.go
+++ src/logic/tree.go
@@ -2,62 +2,41 @@ package logic
 
 // Node represents a game tree node.
 type Node struct {
-	// Value is the value of this node.
+	// Value contains the value of this node.
 	Value [3][3]int
 
-	// Children is a slice of children of this node.
+	// Children is a slice containing children of this node.
 	Children []*Node
 }
 
-// previous is the previous value inserted into children.
-var previous int = X
+// previous represents the most recently inserted value
+var previous int = O
 
-// Tree generates a new game tree for tic-tac-toe.
+// Tree generates a new game tree.
 func Tree() {
-	var tree map[string]*Node = make(map[string]*Node)
-	tree["root"] = &Node{
+	var root *Node = &Node{
 		Value:    [3][3]int{},
 		Children: []*Node{},
 	}
-	generateRootChildren(tree, tree["root"].Value)
+	generateChildren(root)
 }
 
-// generateRootChildren generates all children of the root node and recursively calls generateChildren for each of the root's children.
-func generateRootChildren(tree map[string]*Node, mtx [3][3]int) {
-	var child int
-	for i := 0; i < 3; i++ {
-		for k := 0; k < 3; k++ {
-			var matrix [3][3]int = mtx
-			if matrix[i][k] == EMPTY {
-				matrix[i][k] = X
-			}
-			tree["root"].Children = append(tree["root"].Children, &Node{
-				Value:    matrix,
-				Children: []*Node{},
-			})
-			generateChildren(tree["root"].Children[child])
-			child++
-		}
-	}
-}
-
-// generateChildren generates all children for a child node.
+// generateChildren recursively generates children for a node until the tree is complete.
 func generateChildren(node *Node) {
-	var (
-		child  int
-		insert int = X
-	)
+	var child, player int
 
 	if previous == X {
-		insert = O
+		player = O
+	} else {
+		player = X
 	}
 
 	for i := 0; i < 3; i++ {
 		for k := 0; k < 3; k++ {
 			var matrix [3][3]int = node.Value
 			if matrix[i][k] == EMPTY {
-				matrix[i][k] = insert
-				previous = insert
+				matrix[i][k] = player
+				previous = player
 			}
 			if matrix != node.Value {
 				node.Children = append(node.Children, &Node{
