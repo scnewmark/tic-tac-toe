b96a5452d00572617a0a470167cb03d1e200d2f6
Sap ★ <samuel.newmark10@gmail.com>
2021-02-12 19:26:58 -0600

    Update game tree to create proper number of leaf nodes

--- src/logic/tree.go
+++ src/logic/tree.go
@@ -13,12 +13,13 @@ type Node struct {
 var previous int = O
 
 // Tree generates a new game tree.
-func Tree() {
+func Tree() *Node {
 	var root *Node = &Node{
 		Value:    [3][3]int{},
 		Children: []*Node{},
 	}
 	generateChildren(root)
+	return root
 }
 
 // generateChildren recursively generates children for a node until the tree is complete.
@@ -43,9 +44,44 @@ func generateChildren(node *Node) {
 					Value:    matrix,
 					Children: []*Node{},
 				})
-				generateChildren(node.Children[child])
+				if !winner(matrix) {
+					generateChildren(node.Children[child])
+				}
 				child++
 			}
 		}
 	}
+}
+
+func winner(matrix [3][3]int) bool {
+	for row := 0; row < 3; row++ {
+		if (matrix[row][0] == X && matrix[row][1] == X && matrix[row][2] == X) || (matrix[row][0] == O && matrix[row][1] == O && matrix[row][2] == O) {
+			return true
+		}
+	}
+
+	for col := 0; col < 3; col++ {
+		if (matrix[0][col] == X && matrix[1][col] == X && matrix[2][col] == X) || (matrix[0][col] == O && matrix[1][col] == O && matrix[2][col] == O) {
+			return true
+		}
+	}
+
+	var xtaken, otaken = 0, 0
+	for i := 0; i < 3; i++ {
+		if matrix[i][i] == X {
+			xtaken++
+		} else if matrix[i][i] == O {
+			otaken++
+		}
+	}
+
+	if xtaken == 3 || otaken == 3 {
+		return true
+	}
+
+	if (matrix[0][2] == X && matrix[1][1] == X && matrix[2][0] == X) || (matrix[0][2] == O && matrix[1][1] == O && matrix[2][0] == O) {
+		return true
+	}
+
+	return false
 }
