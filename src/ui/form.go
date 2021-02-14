791089c23e56dc099df321ff6e857af9dac740a1
Sap ★ <samuel.newmark10@gmail.com>
2021-02-14 08:52:13 -0600

    Add light mode support & improve form

--- src/ui/form.go
+++ src/ui/form.go
@@ -1,17 +1,26 @@
 package ui
 
 import (
+	"image/color"
 	"strconv"
 
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 	"github.com/scnewmark/tic-tac-toe/src/logic"
 )
 
 var (
-	sizes  []string = []string{"3x3", "4x4", "5x5", "6x6", "7x7", "8x8", "9x9", "10x10"}
-	pieces []string = []string{"X", "O"}
-	modes  []string = []string{"Single Player", "Two Player"}
+	// Title is the form title.
+	Title *canvas.Text
+
+	// TitleColor is the form title color.
+	TitleColor color.Color = color.White
+
+	sizes []string = []string{"3x3", "4x4", "5x5", "6x6", "7x7", "8x8", "9x9", "10x10"}
+	modes []string = []string{"Single Player", "Two Player"}
 )
 
 // LoadForm creates a new selection form and adds it to the window content.
@@ -34,14 +43,12 @@ func LoadForm(w fyne.Window) {
 			label.Hide()
 		}
 	})
-	pieceMenu := widget.NewSelect(pieces, func(s string) {})
 
 	form := &widget.Form{
 		SubmitText: "Play",
 		Items: []*widget.FormItem{
-			{Text: "Select a piece:", Widget: pieceMenu},
-			{Text: "Select a board:", Widget: sizeMenu},
-			{Text: "Select a mode:", Widget: playerMode},
+			{Text: "Board size:", Widget: sizeMenu},
+			{Text: "Mode:", Widget: playerMode},
 			{Text: "", Widget: label}},
 		OnSubmit: func() {
 			w.Hide()
@@ -62,5 +69,13 @@ func LoadForm(w fyne.Window) {
 		},
 	}
 
-	w.SetContent(form)
+	Title = canvas.NewText("Welcome to Tic Tac Toe", TitleColor)
+
+	Title.TextSize = 30
+	Title.Alignment = fyne.TextAlignCenter
+	Title.TextStyle.Bold = true
+
+	home := container.New(layout.NewVBoxLayout(), widget.NewLabel(""), Title, widget.NewLabel(""), widget.NewSeparator(), widget.NewLabel(""), form)
+
+	w.SetContent(home)
 }
--- src/ui/menu.go
+++ src/ui/menu.go
@@ -1,7 +1,10 @@
 package ui
 
 import (
+	"image/color"
+
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/theme"
 )
 
 // LoadMenu loads the main menu for the game.
@@ -13,8 +16,20 @@ func LoadMenu(w fyne.Window) {
 
 	resetGame := fyne.NewMenuItem("Reset Game", Reset)
 
-	menu := fyne.NewMenu("Game", newGame, resetGame)
-	mainMenu := fyne.NewMainMenu(menu)
+	light := fyne.NewMenuItem("Light Theme", func() {
+		fyne.CurrentApp().Settings().SetTheme(theme.LightTheme())
+		TitleColor = color.Black
+		Title.Color = TitleColor
+	})
+	dark := fyne.NewMenuItem("Dark Theme", func() {
+		fyne.CurrentApp().Settings().SetTheme(theme.DarkTheme())
+		TitleColor = color.White
+		Title.Color = TitleColor
+	})
+
+	gameMenu := fyne.NewMenu("Game", newGame, resetGame)
+	themeMenu := fyne.NewMenu("Theme", light, dark)
+	mainMenu := fyne.NewMainMenu(gameMenu, themeMenu)
 
 	w.SetMainMenu(mainMenu)
 }
